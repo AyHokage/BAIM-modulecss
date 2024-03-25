9f0a34bb76946bc83beb8dc2c3fbb739d42648cb
Diana Baghirova <diana.baghirova18@gmail.com>
2024-03-25 15:15:08 +0400

    switched from plain css to .module.css

---
+++ src/components/PositionRelative/PositionRelative.tsx
@@ -0,0 +1,12 @@
+import NumbersIncrement from '../NumbersIncrement/NumbersIncrement'
+import styles from './PositionRelative.module.css'
+
+const PositionRelative = () => {
+  return (
+    <div className={styles.relative}>
+     <NumbersIncrement />
+    </div>
+  )
+}
+
+export default PositionRelative
